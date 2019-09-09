4c3f9f6551151c2e1b6e713e45757abf18bf7bc3
kharma <kharma@gmail.com>
2019-09-09 11:45:26 -0700

    update/organize modules

--- ndoch-2019/01_modules.py
+++ ndoch-2019/01_modules.py
@@ -3,11 +3,20 @@
 # Pandas modules for working with DataFrames
 import pandas as pd
 
-# Numpy modules for data manipulation
+# Modules for data manipulation
 import numpy as np
+
+# Modules for statistical models
+import scipy
+from scipy import stats
+import statsmodels.api as sm
 
 # Numpy modules for data visualization
 import matplotlib.pyplot as plt
 
-# adjust plot settings to output correctly
+# Adjust plot settings to output correctly
 %matplotlib inline
+
+# Seaborn module for plots; built on Matplotlib package:
+# https://seaborn.pydata.org/
+import seaborn as sns; sns.set(color_codes=True)
--- ndoch-2019/02B_df_shape.py
+++ ndoch-2019/02B_df_shape.py
@@ -1,4 +1,27 @@
 # 02B - Show dataframe shape
 
-# Output dataset size; df is the dataframe variable where data is stored
-df.shape
+print('Unique Values - Route:')
+print(df['Route'].unique())
+print('')
+
+print('Value Counts - Route:')
+print(df['Route'].value_counts())
+print('')
+
+print('Unique Values - County:')
+print(df['County'].unique())
+print('')
+
+print('Value Counts - County:')
+print(df['County'].value_counts())
+print('')
+
+print('Column Dimensions - Including Outliers:')
+print(df.shape)
+print('')
+
+# Remove outlier values, i.e. outside 3 standard deviations
+df_numeric = df_numeric[np.abs(df_numeric['Number of Incidents']- df_numeric['Number of Incidents'].mean()) <= (3*df_numeric['Number of Incidents'].std())]
+
+print('Column Dimensions - Excluding Outliers:')
+print(df_numeric.shape)
