feefa2515db0f40a5570db4d37a687fdb57078bc
kharma <kharma@gmail.com>
2019-09-11 19:07:41 -0700

    organize modules

--- ndoch-2019/02C_df_describe.py
+++
@@ -1,5 +0,0 @@
-# 02C - Summary statistics
-
-# Output summary statistics for dataframe
-print('Summary Statistics - Annual VMT Column:')
-df['Annual_VMT'].describe()
--- ndoch-2019/03B_box_plot.py
+++
@@ -1,8 +0,0 @@
-# 03B - Create box plot for cost by region
-
-# Set plot size and title
-plt.figure(figsize=(16, 12))
-plt.title('Box Plot - Annual VMT by County')
-
-# Create chart
-ax = sns.boxplot(x="County", y="Annual_VMT", data=df)
