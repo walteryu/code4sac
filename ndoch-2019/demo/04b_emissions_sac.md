b635276e6fb35f2385dc923e50b60a46b860ba55
kharma <kharma@gmail.com>
2019-09-21 15:31:50 -0700

    add team demo dir

---
+++ ndoch-2019/demo/04b_emissions_sac.md
@@ -0,0 +1,5 @@
+## Emissions Analysis
+
+Building on the vehicle traffic analysis, we will analyze CARB 2017 vehicle [emission data](https://www.arb.ca.gov/emfac/2017/) for Sacramento County. Data exploration showed that the dataset consists of vehicle type, fuel type, traffic count, vehicle speed, model year and pollutant type.
+
+Data visualization was completed using box plots for basic parameters and scatterplots for larger parameters. 
---
+++ ndoch-2019/demo/04b_emissions_sac.py
@@ -0,0 +1,40 @@
+# load data into notebook; make sure file is in folder as notebook
+em = pd.read_csv('carb_2017_emissions_sac_v1.csv')
+
+# print out first 5 lines of dataset below
+print(em.head(5))
+
+print('Data dimensions:')
+print('')
+print(em.shape())
+print('')
+
+# inspect for data types and missing values (note: Pandas stores character strings as type 'object')
+# print('Dataset information:\n')
+# print(em.info())
+
+# Create box plots
+# Set plot size and title
+plt.figure(figsize=(16, 12))
+plt.title('Box Plot - CO^2 by Vehicle Speed')
+
+# Create chart
+ax = sns.boxplot(x="Speed", y="CO2_RUNEX", data=em)
+
+# Create box plots
+# Set plot size and title
+plt.figure(figsize=(30, 12))
+plt.title('Box Plot - CO^2 by Model Year')
+
+# Create chart
+ax = sns.boxplot(x="Model Year", y="CO2_RUNEX", data=em)
+
+# Create box plots
+# Set plot size and title
+plt.figure(figsize=(30, 12))
+plt.title('Box Plot - CO^2 by VMT')
+
+# Create chart
+ax = sns.scatterplot(x="VMT", y="CO2_RUNEX", data=em)
+
+# Pairs plots
