26184e15270c9a831a69125bea928509f9ac7349
wyu <kharma@gmail.com>
2019-09-29 10:36:44 -0700

    organize repo

--- ndoch-2019/modules/02C_df_describe.py
+++
@@ -1,8 +0,0 @@
-# 02C - Summary statistics
-
-# Output summary statistics for dataframe
-print('Summary Statistics - Annual VMT Column:')
-df['Annual_VMT'].describe()
-
-# Note you can also get summary statistics for the entire dataframe, e.g.
-# df.describe(include='all') # note the `include='all'` argument will include non-numeric columns (e.g. # unique)
