1b16a9991d7d3352ad893aa18669bbba2b6c60f6
wyu <kharma@gmail.com>
2020-01-26 10:24:22 -0800

    rename data/update modules

--- hmis/modules.py
+++ hmis/modules.py
@@ -2,41 +2,41 @@
 
 # load data into notebook; make sure file is in folder as notebook
 df_assessments = pd.read_csv('data/assessments.csv')
-df_clients = pd.read_csv('data/clients.csv')
-df_enrollments = pd.read_csv('data/enrollments.csv')
-df_exits = pd.read_csv('data/exits.csv')
-df_projects = pd.read_csv('data/projects.csv')
-df_services = pd.read_csv('data/services.csv')
+# df_clients = pd.read_csv('data/clients.csv')
+# df_enrollments = pd.read_csv('data/enrollments.csv')
+# df_exits = pd.read_csv('data/exits.csv')
+# df_projects = pd.read_csv('data/projects.csv')
+# df_services = pd.read_csv('data/services.csv')
 
 # example of converting column data type
-# df['Labor Cost'] = pd.to_numeric(df['Labor Cost'])
+# df['cost'] = pd.to_numeric(df['cost'])
 
 # replace white spaces
 # https://stackoverflow.com/questions/13757090/pandas-column-access-w-column-names-containing-spaces
-print("*** column names - assessments ***")
-df_assessments.columns = [c.replace(' ', '_') for c in df_assessments.columns]
-print(list(df_assessments.columns))
-print("")
-print("*** column names - clients ***")
-df_clients.columns = [c.replace(' ', '_') for c in df_clients.columns]
-print(list(df_clients.columns))
-print("")
-print("*** column names - enrollments ***")
-df_enrollments.columns = [c.replace(' ', '_') for c in df_enrollments.columns]
-print(list(df_enrollments.columns))
-print("")
-print("*** column names - exits ***")
-df_exits.columns = [c.replace(' ', '_') for c in df_exits.columns]
-print(list(df_exits.columns))
-print("")
-print("*** column names - projects ***")
-df_projects.columns = [c.replace(' ', '_') for c in df_projects.columns]
-print(list(df_projects.columns))
-print("")
-print("*** column names - services ***")
-df_services.columns = [c.replace(' ', '_') for c in df_services.columns]
-print(list(df_services.columns))
-print("")
+# print("*** column names - assessments ***")
+# df_assessments.columns = [c.replace(' ', '_') for c in df_assessments.columns]
+# print(list(df_assessments.columns))
+# print("")
+# print("*** column names - clients ***")
+# df_clients.columns = [c.replace(' ', '_') for c in df_clients.columns]
+# print(list(df_clients.columns))
+# print("")
+# print("*** column names - enrollments ***")
+# df_enrollments.columns = [c.replace(' ', '_') for c in df_enrollments.columns]
+# print(list(df_enrollments.columns))
+# print("")
+# print("*** column names - exits ***")
+# df_exits.columns = [c.replace(' ', '_') for c in df_exits.columns]
+# print(list(df_exits.columns))
+# print("")
+# print("*** column names - projects ***")
+# df_projects.columns = [c.replace(' ', '_') for c in df_projects.columns]
+# print(list(df_projects.columns))
+# print("")
+# print("*** column names - services ***")
+# df_services.columns = [c.replace(' ', '_') for c in df_services.columns]
+# print(list(df_services.columns))
+# print("")
 
 # print out first 5 lines of dataset below
 # print(df_assessments.head(5))
@@ -50,23 +50,23 @@ print("")
 print("*** column names - assessments ***")
 print(df_assessments.info())
 print("")
-print("*** column names - clients ***")
-print(df_clients.info())
-print("")
-print("*** column names - enrollments ***")
-print(df_enrollments.info())
-print("")
-print("*** column names - exits ***")
-print(df_exits.info())
-print("")
-print("*** column names - projects ***")
-print(df_projects.info())
-print("")
-print("*** column names - services ***")
-print(df_services.info())
-print("")
+# print("*** column names - clients ***")
+# print(df_clients.info())
+# print("")
+# print("*** column names - enrollments ***")
+# print(df_enrollments.info())
+# print("")
+# print("*** column names - exits ***")
+# print(df_exits.info())
+# print("")
+# print("*** column names - projects ***")
+# print(df_projects.info())
+# print("")
+# print("*** column names - services ***")
+# print(df_services.info())
+# print("")
 
-# we see from df.info() that some columns have null/missing values, let's retain only complete records
+# drop null values
 # df = df.dropna()
 
 # 02B - Show dataframe shape
