387de3355d169a054b2411a58f04fbfe517bf5e1
wyu <kharma@gmail.com>
2020-04-05 11:52:10 -0700

    update modules

--- covid/modules/01_modules.py
+++ covid/modules/01_modules.py
@@ -23,8 +23,13 @@ import matplotlib.pyplot as plt
 # Install a pip package in the current Jupyter kernel; run only for initial install:
 # https://jakevdp.github.io/blog/2017/12/05/installing-python-packages-from-jupyter/
 # import sys
-# !{sys.executable} -m pip install seaborn==0.9.0
 # !{sys.executable} -m pip install --upgrade pip
+# !{sys.executable} -m pip install seaborn==0.9.0
+
+# install fbprophet for timeseries
+# !{sys.executable} -m pip install pystan
+# !{sys.executable} -m pip install fbprophet
+# import fbprophet
 
 # seaborn module for plots; built on matplotlib package:
 # https://seaborn.pydata.org/
--- covid/modules/03c_time_series_nyt.py
+++ covid/modules/03c_time_series_nyt.py
@@ -1,14 +1,8 @@
 # 03c - create time series
 
-# Set plot size and title
-plt.figure(figsize=(16, 12))
-plt.title('Time Series: Total CA Cases by County (As of 04-03-2020)')
-
 # Create chart
-ax = sns.tsplot(
-    data=df_california,
-    time="date",
-    unit="cases",
-    condition="county",
-    value="Total Cases"
+ax = df_california.plot(x='date', y='cases')
+ax = plt.title(
+    'Time Series: Total CA Cases by County (As of 04-03-2020)'
 )
+ax = plt.rcParams["figure.figsize"] = (16,12)
