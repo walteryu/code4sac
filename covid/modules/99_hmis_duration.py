4a3c289adca68410a45df8ea1a2954b92b500481
wyu <kharma@gmail.com>
2020-04-04 20:08:20 -0700

    organize modules

--- covid/modules/99_hmis_duration.py
+++
@@ -1,9 +0,0 @@
-# 03B create enrollment-exit time duration column
-
-# reference: https://stackoverflow.com/questions/37840812/pandas-subtracting-two-date-columns-and-the-result-being-an-integer
-enrollments_exits['enrollment_exit_time'] = enrollments_exits['entry_screen_creation_date'].sub(enrollments_exits['project_exit_date'], axis=0)
-print(enrollments_exits.info())
-
-# note: address date typos and null values
-# reference:
-enrollments_exits['enrollment_exit_time'] = enrollments_exits['enrollment_exit_time'] / np.timedelta64(1, 'D')
