4533b0a0b8a44509a57638957162c199e7e3750b
wyu <kharma@gmail.com>
2020-04-11 15:09:51 -0700

    add sorting

--- covid/modules/03a.01_countplot_nyt.py
+++ covid/modules/03a.01_countplot_nyt.py
@@ -6,14 +6,24 @@
 
 # set plot size and title
 plt.figure(figsize=(12, 8))
-plt.title('Count Plot: Total Case Count by US State (As of 04-03-2020)')
+plt.title('Count Plot: Total Case Count by US State')
 
-# create countplot
-sns.countplot(y="state", data=df_total)
+# create countplot and sort categories by value
+# https://stackoverflow.com/questions/46623583/seaborn-countplot-order-categories-by-count
+sns.countplot(
+    y="state",
+    data=df_total,
+    order = df_total['state'].value_counts().index
+)
 
 # set plot size and title
 plt.figure(figsize=(12, 8))
-plt.title('Count Plot: Total Case Count in CA by County (As of 04-03-2020)')
+plt.title('Count Plot: Total Case Count in CA by County')
 
-# create countplot
-sns.countplot(y="county", data=df_california)
+# create countplot and sort categories by value
+# https://stackoverflow.com/questions/46623583/seaborn-countplot-order-categories-by-count
+sns.countplot(
+    y="county",
+    data=df_california,
+    order = df_california['county'].value_counts().index
+)
