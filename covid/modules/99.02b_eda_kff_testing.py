74d952592adc24f930635715910126844cdc8fb9
wyu <kharma@gmail.com>
2020-04-29 20:32:54 -0700

    refactor func/organize modules

---
+++ covid/modules/99.02b_eda_kff_testing.py
@@ -0,0 +1,7 @@
+# 02B.02 - eda/kff data
+# Source data: https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+
+print("*** dataset info - total results by state ***")
+print("")
+print(df_total_kff.info())
+print("")
