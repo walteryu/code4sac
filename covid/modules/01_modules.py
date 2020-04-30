57ed897d8913d9f379d9bc117d5e152c735ce35c
wyu <kharma@gmail.com>
2020-04-29 19:27:52 -0700

    update module

--- covid/modules/01_modules.py
+++ covid/modules/01_modules.py
@@ -18,7 +18,7 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 # adjust plot settings to output correctly
-# %matplotlib inline
+%matplotlib inline
 
 # Install a pip package in the current Jupyter kernel; run only for initial install:
 # https://jakevdp.github.io/blog/2017/12/05/installing-python-packages-from-jupyter/
@@ -26,13 +26,8 @@ import matplotlib.pyplot as plt
 # !{sys.executable} -m pip install --upgrade pip
 # !{sys.executable} -m pip install seaborn==0.9.0
 
-# install fbprophet for timeseries
-# !{sys.executable} -m pip install pystan
-# !{sys.executable} -m pip install fbprophet
-# import fbprophet
-
 # seaborn module for plots; built on matplotlib package:
 # https://seaborn.pydata.org/
 import seaborn as sns; sns.set(color_codes=True)
 
-print('done importing modules')
+print('done importing modules!')
