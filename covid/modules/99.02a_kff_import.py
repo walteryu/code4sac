e9f95db27b136e30e80a14e1f56a15038cc25e58
wyu <kharma@gmail.com>
2020-04-29 20:32:46 -0700

    refactor func/organize modules

---
+++ covid/modules/99.02a_kff_import.py
@@ -0,0 +1,20 @@
+# 02A.02 - Load data into notebook
+# Source data: https://www.kff.org/health-costs/issue-brief/state-data-and-policy-actions-to-address-coronavirus/#stateleveldata
+
+# load data into notebook
+df_total_kff = pd.read_csv('data/kff_state_testing_v1.csv')
+
+print('*** data info - total results by state ***')
+print(df_total_kff.info())
+print("")
+
+print('*** data shape - total results by state ***')
+print(df_total_kff.shape)
+print('')
+
+print('*** summary statistics - total tests with results by state ***')
+print(df_total_kff['tests_with_results'].describe())
+print('')
+
+print('*** summary statistics - total tests per 1k residents by state ***')
+df_total_kff['total_tests_per_1k_residents'].describe()
