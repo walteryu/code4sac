2d36838570746436f374364753a1942be23ae617
wyu <kharma@gmail.com>
2020-05-06 20:27:24 -0700

    refactor into plot func

--- covid/modules/03a.01_countplot_nyt.py
+++ covid/modules/03a.01_countplot_nyt.py
@@ -1,29 +1,26 @@
 # 03A.01 - create count plot
 
-# create histogram
-# `kde=False` turns off the default density estimate plot
-# sns.distplot(clients_enrollments['client_ethnicity'], kde=False)
+def count_plot(title, col, df, idx):
+    # set plot size and title
+    plt.figure(figsize=(12, 8))
+    plt.title(title)
+    # create countplot and sort categories by value
+    # https://stackoverflow.com/questions/46623583/seaborn-countplot-order-categories-by-count
+    sns.countplot(
+        y=col,
+        data=df,
+        order=idx
+    )
 
-# set plot size and title
-plt.figure(figsize=(12, 8))
-plt.title('Count Plot: Total Case Count by US State')
-
-# create countplot and sort categories by value
-# https://stackoverflow.com/questions/46623583/seaborn-countplot-order-categories-by-count
-sns.countplot(
-    y="state",
-    data=df_total,
-    order = df_total['state'].value_counts().index
+count_plot(
+    'Count Plot: Total Case Count by US State',
+    'state',
+    df_total,
+    df_total['state'].value_counts().index
 )
-
-# set plot size and title
-plt.figure(figsize=(12, 8))
-plt.title('Count Plot: Total Case Count in CA by County')
-
-# create countplot and sort categories by value
-# https://stackoverflow.com/questions/46623583/seaborn-countplot-order-categories-by-count
-sns.countplot(
-    y="county",
-    data=df_california,
-    order = df_california['county'].value_counts().index
+count_plot(
+    'Count Plot: Total Case Count in CA by County',
+    "county",
+    df_california,
+    df_california['county'].value_counts().index
 )
--- covid/modules/03a.02_countplot_kff_testing.py
+++ covid/modules/03a.02_countplot_kff_testing.py
@@ -11,30 +11,58 @@
 df_results_plot = df_total_kff[['location', 'tests_with_results']]
 
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/missing_data.html
-# print(pd.isna(df_results_plot))
 df_results_plot['tests_with_results'].fillna(0)
 
 # drop total count to scale plot correctly
 # https://stackoverflow.com/questions/16396903/delete-the-first-three-rows-of-a-dataframe-in-pandas
 df_results_plot = df_results_plot.iloc[1:]
 
-# create histogram
-plt.figure(figsize=(12, 8))
-plt.title('Historgram: Total COVID-19 Tests with Results\n Note: Each Record = Total for Single US State')
-sns.distplot(df_results_plot['tests_with_results'], bins=50)
+# create distplot with settings
+def create_displot(title, df, count):
+    # create histogram
+    plt.figure(figsize=(12, 8))
+    plt.title(title),
+    sns.distplot(df, bins=count)
 
-# create countplot
-plt.figure(figsize=(12, 8))
-plt.title('Bar Plot: Total COVID-19 Tests with Results by US State\n Note: Each Record = Total for Single US State')
+create_displot(
+    'Historgram: Total COVID-19 Tests with Results\n Note: Each Record = Total for Single US State',
+    df_results_plot['tests_with_results'],
+    50
+)
+
+# create histogram
+# plt.figure(figsize=(12, 8))
+# plt.title('Historgram: Total COVID-19 Tests with Results\n Note: Each Record = Total for Single US State')
+# sns.distplot(df_results_plot['tests_with_results'], bins=50)
 
 # aggregate and sort for plot
 # https://gist.github.com/fomightez/bb5a9c727d93d1508187677b4d74d7c1
-df_results_sort = df_results_plot.groupby(["location"])['tests_with_results'].aggregate(np.median).reset_index()
-ax = sns.barplot(
-    y="location",
-    x="tests_with_results",
-    data=df_results_sort
+df_results_sort = df_results_plot.groupby(['location'])['tests_with_results'].aggregate(np.median).reset_index()
+
+# create barplot with settings
+def create_barplot(title, y_col, x_col, df):
+    # create countplot
+    plt.figure(figsize=(12, 8))
+    plt.title(title),
+    ax = sns.barplot(
+        y=y_col,
+        x=x_col,
+        data=df
+    )
+
+create_barplot(
+    'Bar Plot: Total COVID-19 Tests with Results by US State\n Note: Each Record = Total for Single US State',
+    'location',
+    'tests_with_results',
+    df_results_sort
 )
+
+# create_barplot(
+#     'Bar Plot: Total COVID-19 Tests with Results by US State\n Note: Each Record = Total for Single US State',
+#     'location',
+#     'tests_with_results',
+#     df_results_sort
+# )
 
 # subset data and drop null values for plot
 df_positive_plot = df_total_kff[['location', 'percentage_test_positive']]
