25682c0819d84dd13a0d25058709eb5859f48eef
wyu <kharma@gmail.com>
2020-08-23 10:59:54 -0700

    add data filter

--- ndoch_2020/modules/02.01_data_import.py
+++ ndoch_2020/modules/02.01_data_import.py
@@ -62,9 +62,20 @@ df_sfpd = read_data("data/sfpd_report_2003-18.csv")
 # https://gis.data.ca.gov/datasets/CDEGIS::california-schools-2019-20
 df_school = read_data("data/ca_school_2019-20.csv")
 
+# subset dataset by row values; for example, schools by count
+# https://stackoverflow.com/questions/17071871/how-to-select-rows-from-a-dataframe-based-on-column-values
+df_school_sac = df_school_sac[
+    df_school_sac['CountyName'].str.contains('Amador') |
+    df_school_sac['CountyName'].str.contains('Placer') |
+    df_school_sac['CountyName'].str.contains('Sacramento') |
+    df_school_sac['CountyName'].str.contains('Yolo') |
+    df_school_sac['CountyName'].str.contains('Yuba')
+]
+
 # https://opendata.arcgis.com/datasets/f7f818b0aa7a415192eaf66f192bc9cc_0.geojson
 # df_school_geojson = read_data("data/ca_school_2019-20.geojson")
 
 # data profile data after import
 data_profile(df_sfpd, 'SFPD Reports (2003-18)')
 data_profile(df_school, 'CA Schools (2019-20)')
+data_profile(df_school_sac, 'CA Schools: Sac Area (2019-20)')
