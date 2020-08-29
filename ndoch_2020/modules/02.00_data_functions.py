fec0b74b5eba129c37b0189c189e3bca2ddc4b55
wyu <kharma@gmail.com>
2020-08-29 11:47:50 -0700

    place func into module

---
+++ ndoch_2020/modules/02.00_data_functions.py
@@ -0,0 +1,71 @@
+# 02.00 - data functions
+
+# function to read csv file
+# https://stackoverflow.com/questions/32400867/pandas-read-csv-from-url/41880513#41880513
+def read_data(path):
+    df = pd.read_csv(path)
+    return(df)
+
+# function to output csv file
+def output_result(df, filepath):
+    df.to_csv(filepath)
+
+# function to show table info
+def data_profile(df, msg):
+    # pass in variable into string
+    # https://stackoverflow.com/questions/2960772/how-do-i-put-a-variable-inside-a-string
+    print('*** Table Info: %s ***' % msg, '\n')
+    print(df.info(), '\n')
+    print('*** Table Info: Table Dimensions ***', '\n')
+    print(df.shape, '\n')
+
+# function to show unique value for given column
+def show_unique(df, col):
+    # pass in variable into string
+    # https://stackoverflow.com/questions/2960772/how-do-i-put-a-variable-inside-a-string
+    print('*** Unique Values: (%s) ***' % col, '\n')
+    print(df[col].unique(), '\n')
+
+# function to output summary stats
+def summary_stats(df, col):
+    # pass in variable into string
+    # https://stackoverflow.com/questions/2960772/how-do-i-put-a-variable-inside-a-string
+    print('*** Summary Stats: (%s) ***' % col, '\n')
+    print(df[col].describe(), '\n')
+    # print(col.describe())
+
+# function to rename columns
+# https://www.geeksforgeeks.org/how-to-rename-columns-in-pandas-dataframe/
+def rename_col(df, old_col, new_col):
+    df.rename(
+        columns={old_col:new_col},
+        inplace=True
+    )
+    return df
+
+# function convert col to numeric type
+# reference: https://stackoverflow.com/questions/47333227/pandas-valueerror-cannot-convert-float-nan-to-integer
+def convert_num(df, col):
+    # convert type
+    df[col] = pd.to_numeric(
+        df[col],
+        errors='coerce'
+    )
+    return(df)
+
+# convert string to datetime
+# reference: https://stackoverflow.com/questions/32888124/pandas-out-of-bounds-nanosecond-timestamp-after-offset-rollforward-plus-adding-a
+def convert_date(df, col):
+    # convert type
+    df[col] = pd.to_datetime(
+        df[col],
+        infer_datetime_format=True,
+        errors = 'coerce'
+    )
+    return(df)
+
+# function convert col to string type
+def convert_str(df, col):
+    # convert type
+    df[col].astype(str)
+    return(df)
