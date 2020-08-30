5949149fa88bc7b72f543b73416b6c651aaf5d80
wyu <kharma@gmail.com>
2020-08-29 18:44:41 -0700

    organize/rename modules

--- ndoch_2020/modules/03.01_plot_sfpd.py
+++ ndoch_2020/modules/03.01_plot_sfpd.py
@@ -1,4 +1,4 @@
-# 03.01 - map plot
+# 03.01 - map plot: sfpd tutorial
 
 # note: module based on tutorial below
 # https://blog.dominodatalab.com/creating-interactive-crime-maps-with-folium/
@@ -6,24 +6,24 @@
 # sf open data portal - sfpd reports (2003-2018)
 # https://data.sfgov.org/Public-Safety/Police-Department-Incident-Reports-Historical-2003/tmnf-yvry
 
-# set map origin
-SF_COORDINATES = (37.76, -122.45)
+# set origin
+latlong_sf = (37.76, -122.45)
 
-# for speed purposes
-MAX_RECORDS = 1000
+# create map
+map_sfpd = folium.Map(location=latlong_sf, zoom_start=12)
 
-# create empty map zoomed in on San Francisco
-map = folium.Map(location=SF_COORDINATES, zoom_start=16)
+# call function to plot coordinates with cluster markers
+map_sfpd = plot_cluster(
+    df_sfpd.Y,
+    df_sfpd.X,
+    'red',
+    'SFPD: Crime Reports, 2003-2018 (Red)',
+    map_sfpd
+)
 
-# add a marker for every record in the filtered data, use a clustered view
-for each in df_sfpd[0:MAX_RECORDS].iterrows():
-    # note: adjust map settings and update syntax:
-    # https://python-visualization.github.io/folium/quickstart.html
-    folium.Marker(
-        location = [each[1]['Y'],each[1]['X']],
-        clustered_marker = True,
-        popup = 'SFPD Crime Report'
-    ).add_to(map)
-
-display(map)
-map.save('03.01_sfpd_reports.html')
+# call function to add map controls and title
+plot_map(
+    'Crime Report Map: City of San Francisco (2003-2018)',
+    'maps/03.01_sfpd_reports.html',
+    map_sfpd
+)
